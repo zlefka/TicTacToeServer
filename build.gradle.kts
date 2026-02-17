bd1a1a5fa1f9dd9f9cec5d347137493299e24d97
Valeria Udovenchuk <leraudovenchuk@icloud.com>
2026-02-17 13:07:12 +0300

    dependencies updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,23 +15,30 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-    // Основные зависимости Ktor 3.0.0
+
     implementation("io.ktor:ktor-server-core:3.0.0")
     implementation("io.ktor:ktor-server-netty:3.0.0")
     implementation("io.ktor:ktor-server-content-negotiation:3.0.0")
     implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.0")
 
-    // Для логирования
+
     implementation("io.ktor:ktor-server-call-logging:3.0.0")
     implementation("io.ktor:ktor-server-status-pages:3.0.0")
     implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.1")
     implementation("org.apache.logging.log4j:log4j-core:2.24.1")
     implementation("org.apache.logging.log4j:log4j-api:2.24.1")
 
-    // Koin для DI
+
     implementation("io.insert-koin:koin-core:3.5.6")
     implementation("io.insert-koin:koin-ktor:3.5.6")
     implementation("io.insert-koin:koin-logger-slf4j:3.5.6")
+    implementation("org.jetbrains.exposed:exposed-core:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
+    implementation("org.slf4j:slf4j-nop:2.0.9")
+    implementation("org.postgresql:postgresql:42.7.8")
+    implementation("io.ktor:ktor-server-auth:3.0.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.61.0")
 
 }
 
