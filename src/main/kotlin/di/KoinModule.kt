10bcbd541063e3bd6d8e157614a0497ad0edb0c9
Valeria Udovenchuk <leraudovenchuk@icloud.com>
2026-02-18 18:06:31 +0300

    di updated

--- src/main/kotlin/di/KoinModule.kt
+++ src/main/kotlin/di/KoinModule.kt
@@ -2,11 +2,16 @@ package di
 
 import domain.repository.GameRepository
 import datasource.repository.GameRepositoryImpl
+import datasource.repository.UserRepositoryImpl
+import domain.repository.UserRepository
 import domain.service.GameService
-import domain.service.MinimaxService
+import domain.service.MinimaxComputerService
+import domain.service.UserService
 import org.koin.dsl.module
 
 val gameModule = module {
     single<GameRepository> { GameRepositoryImpl() }
-    single<GameService> { MinimaxService(get()) }
+    single<UserRepository> { UserRepositoryImpl() }
+    single<GameService> { MinimaxComputerService(get()) }
+    single<UserService> { UserService(get()) }
 }
